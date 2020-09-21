25bc6ba55e744cd8e9de2996ce3d85d000e98fa2
MikeWalrus <mikeljx@126.com>
2020-09-21 12:35:32 +0800

    Feature added in calc() to tell players whether they have finished.
    set() now has an optional parameter to pass eatCount. Note that parmN
    must be added when calling calc()..
    
    Minor bug fixed in calc(). It should correctly show each
    player's count when using "r" to rematch.

--- erode.h
+++ erode.h
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdarg.h>
 #define WID 6
 #define LEN 6
 #define RIVAL ((*player == 1) ? 2 : 1)
@@ -10,5 +11,5 @@ void reset(int data[][LEN]);
 int ask(int data[][LEN], int * player, bool robot);
 int calc(int data[][LEN], int count[]);
 bool isNear(int data[][LEN], int x, int y, int player);
-int set(int data[][LEN], int x, int y, int player);
+int set(int data[][LEN], int x, int y, int player, int parmN, ...);
 int artidiot(int data[][LEN], int player);
